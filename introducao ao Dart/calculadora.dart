c6135bf2269de94a8b36f9c39223ca2b35f6948f
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-01-30 17:04:15 -0300

    Operacoes Matemáticas

---
+++ introducao ao Dart/calculadora.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  print("Digite o primeiro número:");
+  final num1 = double.parse(stdin.readLineSync()!);
+  print("Digite o segundo número:");
+  final num2 = double.parse(stdin.readLineSync()!);
+
+  final result = sum(num1, num2);
+  print("O resultado da soma é: $result");
+}
+
+double sum(double num1, double num2) {
+  return num1 + num2;
+}
---
+++ introducao ao Dart/main.dart
@@ -0,0 +1,23 @@
+void main() {
+  // tipo nome
+  var name = 'Anderson';
+  String lastName = 'Choren';
+  dynamic value = 12;
+
+  // var VS dynamic
+  name = 'João'; // Mudando o valor
+  // name = 12; Não posso mudar o tipo do dado
+  value = '32';
+
+  print(value);
+  //print(name+" "+lastName);
+  print('$name $lastName'); // Interpolação de variáveis
+  print('O valor é $value');
+
+  // Constantes no Dart (const e final)
+  const int age = 31;
+  // age = 32;
+
+  final DateTime now = DateTime.now();
+  // now = DateTime(2022);
+}
