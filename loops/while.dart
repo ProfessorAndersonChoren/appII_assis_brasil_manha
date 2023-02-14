8ec36f18a8d1e5cc8d161ec2101b1952114098b2
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-02-14 10:57:24 -0300

    While - completo

--- loops/while.dart
+++ loops/while.dart
@@ -9,25 +9,36 @@ void main() {
   print('0 - Sair');
   int opcao = int.parse(stdin.readLineSync()!);
 
-  while (opcao != 0) {
-    repetir(opcao);
-  }
+  repetir(opcao);
 }
 
 void repetir(int opcao) {
-  print('Digite o primeiro número');
-  double numero1 = double.parse(stdin.readLineSync()!);
-  print('Digite o segundo número');
-  double numero2 = double.parse(stdin.readLineSync()!);
+  while (opcao != 0) {
+    print('Digite o primeiro número');
+    double numero1 = double.parse(stdin.readLineSync()!);
+    print('Digite o segundo número');
+    double numero2 = double.parse(stdin.readLineSync()!);
 
-  switch (opcao) {
-    case 1:
-      double resultado = somar(numero1, numero2);
-      print('A soma é $resultado');
-      break;
-    case 2:
-      double resultado = subtrair(numero1, numero2);
-      print('A subtração é $resultado');
+    switch (opcao) {
+      case 1:
+        double resultado = somar(numero1, numero2);
+        print('A soma é $resultado');
+        break;
+      case 2:
+        double resultado = subtrair(numero1, numero2);
+        print('A subtração é $resultado');
+        break;
+      case 3:
+        double resultado = multiplicar(numero1, numero2);
+        print('A multiplicação é: $resultado');
+        break;
+      case 4:
+        double resultado = dividir(numero1, numero2);
+        print('A divisão é $resultado');
+        break;
+    } // Switch
+    print('Digite uma nova operação:');
+    opcao = int.parse(stdin.readLineSync()!);
   }
 }
 
