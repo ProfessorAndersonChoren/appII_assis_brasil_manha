8256c7220bbf6e3c6ad680917a76c60cf3c3f1c9
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-02-16 12:08:07 -0300

    Revisão Loops

---
+++ loops_revisao/atividade3.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+void main() {
+  List<int> A = List.generate(10, (_) => Random().nextInt(50));
+  List<int> B = [];
+
+  for (int i = 0; i < A.length; i++) {
+    B.add(
+      A[i]
+    );
+  }
+  print(A);
+  print(B);
+}
